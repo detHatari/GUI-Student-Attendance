14fba07f5b0a42e0ddf5de02b7d0abf9e786378f
detHatari <149781504+detHatari@users.noreply.github.com>
2023-11-20 14:54:15 +0700

    Update logincontroller.java

--- logincontroller.java
+++ logincontroller.java
@@ -27,99 +27,6 @@ public class logincontroller {
     @FXML
     private Label loginMessagelabel;
 
-//    @FXML
-//    private MenuButton menuButton;
-
-//    public void onLoginAs(ActionEvent event) {
-//        String selectedRole = menuButton.getText();
-//
-//        if (selectedRole.equals("Teacher")) {
-//            // Handle Teacher login
-//        } else if (selectedRole.equals("Student")) {
-//            // Load StudentView.fxml for Student login
-//            try {
-//                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/attendancesystem/student/StudentView.fxml"));
-//                Parent root = loader.load();
-//
-////                 StudentController studentController = loader.getController();
-//
-//                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//                Scene scene = new Scene(root);
-//                stage.setTitle("Student Interface");
-//                stage.setScene(scene);
-//                stage.show();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
-
-//    public void onLogin(ActionEvent event) throws IOException {
-//        String adminUsername = "Admin";
-//        String adminPassword = "admin123";
-//        List<User> users = Userdata.loadUsers();
-//
-//        String enterUsername = usernameTextField.getText();
-//        String enterPassword = passwordField.getText();
-//        if (!usernameTextField.getText().isBlank() && !passwordField.getText().isBlank()) {
-//            if (enterUsername.equals(adminUsername) && enterPassword.equals(adminPassword)) {
-//                loginMessagelabel.setText("You are trying to login");
-//                Parent root = FXMLLoader.load(LoginFormApp.class.getResource("hello-view.fxml"));
-//                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//                Scene scene = new Scene(root);
-//                stage.setTitle("View");
-//                stage.setWidth(stage.getWidth());
-//                stage.setHeight(stage.getHeight());
-//                stage.setScene(scene);
-//                stage.show();
-//            }
-//            else if (users != null) {
-//                for (User user : users) {
-//                    if (user.getUsername().equals(enterUsername) && user.getPassword().equals(enterPassword)) {
-//                        loginMessagelabel.setText("You are trying to login");
-//                        Parent root = FXMLLoader.load(LoginFormApp.class.getResource("hello-view.fxml"));
-//                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//                        Scene scene = new Scene(root);
-//                        stage.setTitle("View");
-//                        stage.setWidth(stage.getWidth());
-//                        stage.setHeight(stage.getHeight());
-//                        stage.setScene(scene);
-//                        stage.show();
-//                    }
-//                    else {
-//                        loginMessagelabel.setText("Incorrect username and password");
-//                    }
-//                }
-//            }
-//        }
-////        else if (users !=null) {
-////            for (User user : users){
-////                if(user.getUsername().equals(enterUsername) && user.getPassword().equals(enterPassword)){
-////                    loginMessagelabel.setText("You are trying to login");
-////                    Parent root = FXMLLoader.load(LoginFormApp.class.getResource("hello-view.fxml"));
-////                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-////                    Scene scene = new Scene(root);
-////                    stage.setTitle("View");
-////                    stage.setWidth(stage.getWidth());
-////                    stage.setHeight(stage.getHeight());
-////                    stage.setScene(scene);
-////                    stage.show();
-////                }
-////                else {
-////                    loginMessagelabel.setText("Incorrect username and password");
-////                }
-////            }
-////        }
-//        else {
-//            loginMessagelabel.setText("Please enter username and password");
-//        }
-//    }
-
-
-
-
-
 
     public void onLogin(ActionEvent event) throws IOException {
         String adminUsername = "Admin";
@@ -164,34 +71,6 @@ public class logincontroller {
         stage.setScene(scene);
         stage.show();
     }
-//    public void onLogin(ActionEvent event) throws IOException {
-////        String adminUsername = "Admin";
-////        String adminPassword = "admin123";
-//        List<User> users = Userdata.loadUsers();
-//
-//        String username = usernameTextField.getText();
-//        String password = passwordField.getText();
-//        if(users !=null){
-//            for(User user: users){
-//                if(user.getUsername().equals(username) && user.getPassword().equals(password)){
-//                    loginMessagelabel.setText("Login success");
-//                    openMainView(event);
-//                }
-//                else if (username.equals("admin") && password.equals("123")) {
-//                    loginMessagelabel.setText("Login success");
-//                    openMainView(event);
-//                }
-//            }
-//        }
-//        if (username.isEmpty() || password.isEmpty()) {
-//            loginMessagelabel.setText("Please enter both username and password.");
-//        } else {
-//            loginMessagelabel.setText("Wrong username or password");
-//        }
-
-//    }
-
-
 
 
     public void onResgister(ActionEvent event) throws IOException {
